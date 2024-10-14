5cf35d01f233b42339b822d4e93f9ae59dfee4b5
sergey91sergeev <sergey91sergeev@gmail.com>
2024-10-14 16:14:48 +0300

    Первый коммит

---
+++ bot.py
@@ -0,0 +1,45 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram import executor
+from config import TOKEN
+from db_sqlite3 import insert_into_table_User
+from db_sqlite3 import insert_into_table_City
+from requests_weather import get_weather
+import asyncio
+import datetime
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
+
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_surname = message.from_user.last_name
+    username = message.from_user.username
+    date_time = datetime.datetime.today()
+
+    insert_into_table_User(user_id, user_name, user_surname, username, date_time)
+
+@dp.message_handler()
+async def weather_handler(message: types.Message):
+    try:
+        city = message.text.strip()
+        weather = get_weather(city)
+
+        await message.reply(f"Погода в {city}:\n{weather}")
+
+        user_id = message.from_user.id
+        insert_into_table_City(user_id, city)
+
+    except IndexError:
+        await message.reply("Используй формат: /weather [город]")
+
+
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
---
+++ requests_weather.py
@@ -0,0 +1,43 @@
+import requests
+from config import API_KEY
+
+weather_api_key = API_KEY
+
+
+def get_weather(city: str) -> str:
+    """Функция принимает название города и возвращает сводку по погоде"""
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=ru"
+    response = requests.get(url).json()
+    if response.get("cod") != 200:
+        return "Город не найден."
+
+    weather_description = response["weather"][0]["description"].capitalize()
+    temperature = response["main"]["temp"]
+    speed = response["wind"]["speed"]
+    clouds = response["clouds"]["all"]
+    humidity = response["main"]["humidity"]
+    feels_like = response["main"]["feels_like"]
+    weather_condition = response["weather"][0]["main"].lower()
+    icon = get_weather_icon(weather_condition)
+
+    return (f"{weather_description} {icon}\n"
+            f"Температура {round(temperature, 1)} °C\n"
+            f"Ощущается как {round(feels_like, 1)} °C\n"
+            f"Ветер {round(speed, 1)} м/с\n"
+            f"Облачность {clouds} %\n"
+            f"Влажность {humidity} %\n")
+
+def get_weather_icon(weather_condition: str) -> str:
+    """Функция принимает параметр weather_condition (погодные условия)
+    и возвращает эмодзи, соответствующий условиям"""
+    weather_icons = {
+        "clear": "\U00002600",  # ☀️
+        "clouds": "\U00002601",  # ☁️
+        "rain": "\U0001F327",  # 🌧
+        "thunderstorm": "\U0001F329",  # 🌩
+        "snow": "\U0001F328",  # 🌨
+        "fog": "\U0001F32B",  # 🌫
+        "partly cloudy": "\U000026C5",  # ⛅
+        "windy": "\U0001F32C"  # 🌬
+    }
+    return weather_icons.get(weather_condition)
